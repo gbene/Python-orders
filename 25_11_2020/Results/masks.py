f5e4e47ad32dd295689bf71b2d24132d659dc4ae
gbene <40857804+gbene@users.noreply.github.com>
2020-11-26 09:58:12 +0100

    Update masks.py

--- 25_11_2020/Results/masks.py
+++ 25_11_2020/Results/masks.py
@@ -1,11 +1,8 @@
 '''
 author: Gabriele Benedetti
-
 25/11/2020
-
 aim:
 Apply certain masks to a specific range of images:
-
 photo 1: '' Links_Initial.jpg ''
 photo 2 to 21: '' Links_Regular.jpg ''
 photo 22: '' Midden_Initial.jpg ''
@@ -23,14 +20,14 @@ cameras = chunk.cameras #define the cameras in the chunk
 
 img_path = mt.app.getExistingDirectory() # ask the folder path of the masks
 tol = mt.app.getInt("Choose tolerance value for masking") #ask for the masking tolerance value
-os.chdir(img_path) #change directory to that path
+     #change directory to that path
 
 # EXECUTION
 
 
-chunk.importMasks(path='Links_Initial.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement, tolerance=tol,cameras=cameras[0])
-chunk.importMasks(path='Links_Regular.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement,tolerance=tol,cameras=cameras[1:21])
-chunk.importMasks(path='Midden_Initial.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement,tolerance=tol,cameras=cameras[21])
-chunk.importMasks(path='Midden_Regular.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement,tolerance=tol,cameras=cameras[22:42])
-chunk.importMasks(path='Rechts_Initial.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement,tolerance=tol,cameras=cameras[42])
-chunk.importMasks(path='Rechts_Regular.jpg',source=MaskSourceBackground,operation=MaskOperationReplacement,tolerance=tol,cameras=cameras[43:64])
+chunk.importMasks(path=os.path.join(img_path,'Links_Initial.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[0])
+chunk.importMasks(path=os.path.join(img_path,'Links_Regular.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[1:21])
+chunk.importMasks(path=os.path.join(img_path,'Midden_Initial.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[21])
+chunk.importMasks(path=os.path.join(img_path,'Midden_Regular.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[22:42])
+chunk.importMasks(path=os.path.join(img_path,'Rechts_Initial.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[42])
+chunk.importMasks(path=os.path.join(img_path,'Rechts_Regular.jpg'),source=Metashape.MaskSource.MaskSourceBackground,operation=Metashape.MaskOperation.MaskOperationReplacement,tolerance=tol,cameras=cameras[43:64])
