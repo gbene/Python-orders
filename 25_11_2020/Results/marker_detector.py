657c22081a98af35567890e2b3feeb35266a0380
gbene <40857804+gbene@users.noreply.github.com>
2020-12-07 14:57:49 +0100

    Update marker_detector.py

--- 25_11_2020/Results/marker_detector.py
+++ 25_11_2020/Results/marker_detector.py
@@ -5,7 +5,7 @@ chunk = mt.app.document.chunk #define active chunk
 markers = chunk.markers #define present markers
 total_targets = 0 #intitial condition
 
-ref_file = mt.app.getOpenFileName("Choose reference coordinate file")
+#ref_file = mt.app.getOpenFileName("Choose reference coordinate file")
 
 # Loop that cicles every marker present and counts the respective projections
 for marker in markers:
@@ -20,4 +20,4 @@ if total_targets == 135:
 else:
 	print("NOTE: The number of GCPs detected is different from 135")
 
-chunk.importReference(path=ref_file, format=Metashape.ReferenceFormatCSV, columns='nxyz', delimiter=';')
+#chunk.importReference(path=ref_file, format=Metashape.ReferenceFormatCSV, columns='nxyz', delimiter=';')
